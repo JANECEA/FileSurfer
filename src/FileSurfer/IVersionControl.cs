2f5a17f04469e4556be0f329ccd5b623d6abd289
JANECEA <adam-janecek@seznam.cz>
2024-07-20 22:13:02 +0200

    extend IVersionControl.cs

--- src/FileSurfer/IVersionControl.cs
+++ src/FileSurfer/IVersionControl.cs
@@ -4,9 +4,15 @@ public interface IVersionControl
 {
     public bool IsVersionControlled(string directoryPath);
 
+    public string[] GetUnstagedFiles();
+
     public bool DownloadChanges(out string? errorMessage);
-    
-    public string[] GetBranches(out string? errorMessage);
+
+    public string[] GetBranches();
+
+    public string GetCurrentBranchName();
+
+    public bool SwitchBranches(string branchName, out string? errorMessage);
 
     public bool StageChange(string filePath, out string? errorMessage);
 
