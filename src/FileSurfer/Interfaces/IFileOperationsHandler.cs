0ac9ad4519ec44bfe47c29a1390f359509280827
JANECEA <adam-janecek@seznam.cz>
2024-07-23 20:41:08 +0200

    add IFileOperationsHandler.cs

--- src/FileSurfer/Interfaces/IFileOperationsHandler.cs
+++ src/FileSurfer/Interfaces/IFileOperationsHandler.cs
@@ -2,5 +2,40 @@ namespace FileSurfer;
 
 public interface IFileOperationsHandler
 {
+    public string[] GetDrives(out string? errorMessage);
 
+    public bool OpenFile(string filePath, out string? errorMessage);
+
+    public string[] GetDirFiles(string dirPath, out string? errorMessage);
+
+    public string[] GetDirFolders(string dirPath, out string? errorMessage);
+
+    public long? GetFileSizeKiB(string path, out string? errorMessage);
+
+    public object GetFileIcon(string path, out string? errorMessage);
+
+    public object GetFileContextMenu(string path, out string? errorMessage);
+
+    public bool OpenCmdAt(string dirPath, out string? errorMessage);
+
+    public bool NewFileAt(string dirPath, string fileName, out string? errorMessage);
+
+    public bool NewDirAt(string dirPath, string DirName, out string? errorMessage);
+
+    public bool CopyFileToSystemClipBoard(string filePath, out string? errorMessage);
+
+    public bool PasteFileFromClipBoardAt(string filePath, out string? errorMessage);
+
+    public bool RenameFileAt(string filePath, string newName, out string? errorMessage);
+
+    public bool RenameDirAt(string filePath, string newName, out string? errorMessage);
+
+    public bool MoveFileToTrash(string filePath, out string? errorMessage);
+
+    public bool MoveDirToTrash(string dirPath, out string? errorMessage);
+
+    public bool DeleteFile(string filePath, out string? errorMessage);
+
+    public bool DeleteDirectory(string dirPath, out string? errorMessage);
 }
+
