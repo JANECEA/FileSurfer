53b9a59667cc888583c4c88805c9212400d911e1
JANECEA <adam-janecek@seznam.cz>
2024-08-25 15:10:48 +0200

    Change json to settings parsing

--- src/FileSurfer/FileSurferSettings.cs
+++ src/FileSurfer/FileSurferSettings.cs
@@ -1,6 +1,5 @@
 using System;
 using System.Collections.Generic;
-using System.Dynamic;
 using System.IO;
 using System.Text.Json;
 
@@ -9,7 +8,7 @@ namespace FileSurfer;
 public enum DisplayModeEnum
 {
     ListView,
-    IconView
+    IconView,
 }
 
 public enum SortBy
@@ -17,43 +16,42 @@ public enum SortBy
     Name,
     Date,
     Type,
-    Size
+    Size,
 }
-
-internal record SettingsRecord(
-    bool UseDarkMode,
-    bool OpenInLastLocation,
-    string OpenIn,
-    int FileSizeDisplayLimit,
-    string DisplayMode,
-    string DefaultSort,
-    bool SortReversed,
-    bool ShowSpecialFolders,
-    bool ShowProtectedFiles,
-    bool ShowHiddenFiles,
-    bool TreatDotFilesAsHidden,
-    bool GitIntegration,
-    bool ShowUndoRedoErrorDialogs,
-    bool AllowImagePastingFromClipboard,
-    string NewImageName,
-    string NewFileName,
-    string NewDirectoryName,
-    string ThisPCLabel,
-    List<string> QuickAccess
-);
 
 static class FileSurferSettings
 {
-    private static readonly JsonSerializerOptions serializerOptions = new() { WriteIndented = true };
+    internal record SettingsRecord(
+        bool UseDarkMode,
+        bool OpenInLastLocation,
+        string OpenIn,
+        int FileSizeDisplayLimit,
+        string DisplayMode,
+        string DefaultSort,
+        bool SortReversed,
+        bool ShowSpecialFolders,
+        bool ShowProtectedFiles,
+        bool ShowHiddenFiles,
+        bool TreatDotFilesAsHidden,
+        bool GitIntegration,
+        bool ShowUndoRedoErrorDialogs,
+        bool AllowImagePastingFromClipboard,
+        string NewImageName,
+        string NewFileName,
+        string NewDirectoryName,
+        string ThisPCLabel,
+        List<string> QuickAccess
+    );
+
+    private static readonly JsonSerializerOptions serializerOptions =
+        new() { WriteIndented = true };
     private static readonly string _settingsFileDir =
-        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
-        + "\\FileSurfer";
+        Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\FileSurfer";
     public static readonly string SettingsFilePath = _settingsFileDir + "\\settings.json";
     private static string _previousSettingsjson = string.Empty;
 
     public static bool UseDarkMode { get; set; } = true;
     public static bool OpenInLastLocation { get; set; } = true;
-    public static string OpenIn { get; set; } = "";
     public static int FileSizeDisplayLimit { get; set; } = 4096;
     public static DisplayModeEnum DisplayMode { get; set; } = DisplayModeEnum.ListView;
     public static SortBy DefaultSort { get; set; } = SortBy.Name;
@@ -69,6 +67,7 @@ static class FileSurferSettings
     public static string NewFileName { get; set; } = "New File";
     public static string NewDirectoryName { get; set; } = "New Folder";
     public static string ThisPCLabel { get; set; } = "This PC";
+    public static string OpenIn { get; set; } = ThisPCLabel;
     public static List<string> QuickAccess { get; set; } = new List<string>();
 
     public static void LoadSettings()
@@ -77,30 +76,33 @@ static class FileSurferSettings
             SaveSettings();
 
         _previousSettingsjson = File.ReadAllText(SettingsFilePath);
-        dynamic settings =
-            JsonSerializer.Deserialize<ExpandoObject>(_previousSettingsjson) ?? throw new ArgumentNullException();
+
+        SettingsRecord settings =
+            JsonSerializer.Deserialize<SettingsRecord>(_previousSettingsjson)
+            ?? throw new NullReferenceException();
 
         try
         {
-            UseDarkMode = Convert.ToBoolean(settings.UseDarkMode.ToString());
-            OpenInLastLocation = Convert.ToBoolean(settings.OpenInLastLocation.ToString());
-            OpenIn = settings.OpenIn.ToString();
-            FileSizeDisplayLimit = Convert.ToInt32(settings.FileSizeDisplayLimit.ToString());
-            DisplayMode = (DisplayModeEnum)Enum.Parse(typeof(DisplayModeEnum), settings.DisplayMode.ToString());
-            DefaultSort = (SortBy)Enum.Parse(typeof(SortBy), settings.DefaultSort.ToString());
-            SortReversed = Convert.ToBoolean(settings.SortReversed.ToString());
-            ShowSpecialFolders = Convert.ToBoolean(settings.ShowSpecialFolders.ToString());
-            ShowProtectedFiles = Convert.ToBoolean(settings.ShowProtectedFiles.ToString());
-            ShowHiddenFiles = Convert.ToBoolean(settings.ShowHiddenFiles.ToString());
-            TreatDotFilesAsHidden = Convert.ToBoolean(settings.TreatDotFilesAsHidden.ToString());
-            GitIntegration = Convert.ToBoolean(settings.GitIntegration.ToString());
-            ShowUndoRedoErrorDialogs = Convert.ToBoolean(settings.ShowUndoRedoErrorDialogs.ToString());
-            AllowImagePastingFromClipboard = Convert.ToBoolean(settings.AllowImagePastingFromClipboard.ToString());
-            NewImageName = settings.NewImageName.ToString();
-            NewFileName = settings.NewFileName.ToString();
-            NewDirectoryName = settings.NewDirectoryName.ToString();
-            ThisPCLabel = settings.ThisPCLabel.ToString();
-            QuickAccess = JsonSerializer.Deserialize<List<string>>(settings.QuickAccess.ToString());
+            UseDarkMode = settings.UseDarkMode;
+            OpenInLastLocation = settings.OpenInLastLocation;
+            OpenIn = settings.OpenIn;
+            FileSizeDisplayLimit = settings.FileSizeDisplayLimit;
+            DisplayMode = (DisplayModeEnum)
+                Enum.Parse(typeof(DisplayModeEnum), settings.DisplayMode);
+            DefaultSort = (SortBy)Enum.Parse(typeof(SortBy), settings.DefaultSort);
+            SortReversed = settings.SortReversed;
+            ShowSpecialFolders = settings.ShowSpecialFolders;
+            ShowProtectedFiles = settings.ShowProtectedFiles;
+            ShowHiddenFiles = settings.ShowHiddenFiles;
+            TreatDotFilesAsHidden = settings.TreatDotFilesAsHidden;
+            GitIntegration = settings.GitIntegration;
+            ShowUndoRedoErrorDialogs = settings.ShowUndoRedoErrorDialogs;
+            AllowImagePastingFromClipboard = settings.AllowImagePastingFromClipboard;
+            NewImageName = settings.NewImageName;
+            NewFileName = settings.NewFileName;
+            NewDirectoryName = settings.NewDirectoryName;
+            ThisPCLabel = settings.ThisPCLabel;
+            QuickAccess = settings.QuickAccess;
         }
         catch
         {
@@ -110,28 +112,30 @@ static class FileSurferSettings
 
     public static void SaveSettings()
     {
-        SettingsRecord settingsObject = new(
-            UseDarkMode,
-            OpenInLastLocation,
-            OpenIn,
-            FileSizeDisplayLimit,
-            DisplayMode.ToString(),
-            DefaultSort.ToString(),
-            SortReversed,
-            ShowSpecialFolders,
-            ShowProtectedFiles,
-            ShowHiddenFiles,
-            TreatDotFilesAsHidden,
-            GitIntegration,
-            ShowUndoRedoErrorDialogs,
-            AllowImagePastingFromClipboard,
-            NewImageName,
-            NewFileName,
-            NewDirectoryName,
-            ThisPCLabel,
-            QuickAccess
-        );
-        string settingsJson = JsonSerializer.Serialize(settingsObject, serializerOptions);
+        SettingsRecord settings =
+            new(
+                UseDarkMode,
+                OpenInLastLocation,
+                OpenIn,
+                FileSizeDisplayLimit,
+                DisplayMode.ToString(),
+                DefaultSort.ToString(),
+                SortReversed,
+                ShowSpecialFolders,
+                ShowProtectedFiles,
+                ShowHiddenFiles,
+                TreatDotFilesAsHidden,
+                GitIntegration,
+                ShowUndoRedoErrorDialogs,
+                AllowImagePastingFromClipboard,
+                NewImageName,
+                NewFileName,
+                NewDirectoryName,
+                ThisPCLabel,
+                QuickAccess
+            );
+        string settingsJson = JsonSerializer.Serialize(settings, serializerOptions);
+
         if (!Directory.Exists(_settingsFileDir))
             Directory.CreateDirectory(_settingsFileDir);
 
