3f134506fb439baffccc29369cdd3d128d35b0b5
JANECEA <adam-janecek@seznam.cz>
2024-12-14 17:43:33 +0100

    Add tests

---
+++ src/FSTests/ShellInteractionTests.cs
@@ -0,0 +1,50 @@
+using FileSurfer.Models;
+
+namespace FSTests;
+
+public class ShellInteractionTests
+{
+    private readonly WindowsFileIOHandler _fileIOHandler = new(100);
+
+    [Fact]
+    public void ExecuteCmd_Should_Not_Hang()
+    {
+        // Arrange
+        string command = "for /L %i in (1,1,1000) do @echo Line %i"; // Generates 1000 lines of output
+
+        // Act
+        bool result = _fileIOHandler.ExecuteCmd(command, out string? errorMessage);
+
+        // Assert
+        Assert.True(result, "The command should complete successfully in cca 80ms.");
+        Assert.Null(errorMessage);
+    }
+
+    [Fact]
+    public void ExecuteCmd_Should_Return_False()
+    {
+        // Arrange
+        string command = "ping --invalidoption";
+
+        // Act
+        bool result = _fileIOHandler.ExecuteCmd(command, out string? errorMessage);
+
+        // Assert
+        Assert.False(result, "The command should fail.");
+        Assert.NotNull(errorMessage);
+    }
+
+    [Fact]
+    public void ExecuteCmd_Should_Return_True()
+    {
+        // Arrange
+        string command = "echo \"Hello world!\"";
+
+        // Act
+        bool result = _fileIOHandler.ExecuteCmd(command, out string? errorMessage);
+
+        // Assert
+        Assert.True(result, "The command should succeed.");
+        Assert.Null(errorMessage);
+    }
+}
