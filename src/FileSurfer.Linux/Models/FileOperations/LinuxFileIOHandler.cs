cf7f716d32bf2c9154ea7d5975e76916b380abd8
janecea <adam-janecek@seznam.cz>
2025-12-24 19:34:19 +0100

    Linux fileIOHandler first draft

--- src/FileSurfer.Linux/Models/FileOperations/LinuxFileIOHandler.cs
+++ src/FileSurfer.Linux/Models/FileOperations/LinuxFileIOHandler.cs
@@ -29,12 +29,7 @@ public class LinuxFileIOHandler : IFileIOHandler
             if (!File.Exists(filePath))
                 return SimpleResult.Error($"Could not find file: \"{filePath}\"");
 
-            FileSystem.DeleteFile(
-                filePath,
-                UIOption.OnlyErrorDialogs,
-                RecycleOption.DeletePermanently,
-                UICancelOption.ThrowException
-            );
+            File.Delete(filePath);
             return SimpleResult.Ok();
         }
         catch (Exception ex)
@@ -50,12 +45,7 @@ public class LinuxFileIOHandler : IFileIOHandler
             if (!Directory.Exists(dirPath))
                 return SimpleResult.Error($"Could not find directory: \"{dirPath}\"");
 
-            FileSystem.DeleteDirectory(
-                dirPath,
-                UIOption.OnlyErrorDialogs,
-                RecycleOption.DeletePermanently,
-                UICancelOption.ThrowException
-            );
+            Directory.Delete(dirPath, true);
             return SimpleResult.Ok();
         }
         catch (Exception ex)
@@ -109,13 +99,11 @@ public class LinuxFileIOHandler : IFileIOHandler
 
     private static bool IsValidFileName(string fileName) =>
         !string.IsNullOrWhiteSpace(fileName)
-        && Path.GetInvalidFileNameChars()
-            .All(ch => !fileName.Contains(ch, StringComparison.OrdinalIgnoreCase));
+        && Path.GetInvalidFileNameChars().All(ch => !fileName.Contains(ch, PathTools.Comparison));
 
     private static bool IsValidDirName(string dirName) =>
         !string.IsNullOrWhiteSpace(dirName)
-        && Path.GetInvalidPathChars()
-            .All(ch => !dirName.Contains(ch, StringComparison.OrdinalIgnoreCase));
+        && Path.GetInvalidPathChars().All(ch => !dirName.Contains(ch, PathTools.Comparison));
 
     public IResult RenameFileAt(string filePath, string newName)
     {
@@ -125,25 +113,10 @@ public class LinuxFileIOHandler : IFileIOHandler
         try
         {
             if (Path.GetDirectoryName(filePath) is not string parentDir)
-                return SimpleResult.Error($"No parent directory found for \"{filePath}\"");
+                return SimpleResult.Error($"File \"{filePath}\" has no parent directory.");
 
-            string fileName = Path.GetFileName(filePath);
-            if (string.Equals(fileName, newName, StringComparison.OrdinalIgnoreCase))
-            {
-                string tempName = FileNameGenerator.GetAvailableName(
-                    parentDir,
-                    Path.GetRandomFileName()
-                );
-                string tempPath = Path.Combine(parentDir, tempName);
-                File.Move(filePath, tempPath);
-                filePath = tempPath;
-            }
-            FileSystem.MoveFile(
-                filePath,
-                Path.Combine(parentDir, newName),
-                UIOption.AllDialogs,
-                UICancelOption.ThrowException
-            );
+            if (!string.Equals(Path.GetFileName(filePath), newName, PathTools.Comparison))
+                File.Move(filePath, Path.Combine(parentDir, newName));
 
             return SimpleResult.Ok();
         }
@@ -163,23 +136,8 @@ public class LinuxFileIOHandler : IFileIOHandler
             if (Path.GetDirectoryName(dirPath) is not string parentDir)
                 return SimpleResult.Error($"\"{dirPath}\" is a root directory");
 
-            string dirName = Path.GetFileName(dirPath);
-            if (string.Equals(dirName, newName, StringComparison.OrdinalIgnoreCase))
-            {
-                string tempName = FileNameGenerator.GetAvailableName(
-                    parentDir,
-                    Path.GetRandomFileName()
-                );
-                string tempPath = Path.Combine(parentDir, tempName);
-                Directory.Move(dirPath, tempPath);
-                dirPath = tempPath;
-            }
-            FileSystem.MoveDirectory(
-                dirPath,
-                Path.Combine(parentDir, newName),
-                UIOption.AllDialogs,
-                UICancelOption.ThrowException
-            );
+            if (!string.Equals(Path.GetFileName(dirPath), newName, PathTools.Comparison))
+                Directory.Move(dirPath, Path.Combine(parentDir, newName));
 
             return SimpleResult.Ok();
         }
@@ -194,12 +152,7 @@ public class LinuxFileIOHandler : IFileIOHandler
         try
         {
             string newFilePath = Path.Combine(destinationDir, Path.GetFileName(filePath));
-            FileSystem.MoveFile(
-                filePath,
-                newFilePath,
-                UIOption.AllDialogs,
-                UICancelOption.ThrowException
-            );
+            File.Move(filePath, newFilePath);
             return SimpleResult.Ok();
         }
         catch (Exception ex)
@@ -213,12 +166,7 @@ public class LinuxFileIOHandler : IFileIOHandler
         try
         {
             string newDirPath = Path.Combine(destinationDir, Path.GetFileName(dirPath));
-            FileSystem.MoveDirectory(
-                dirPath,
-                newDirPath,
-                UIOption.AllDialogs,
-                UICancelOption.ThrowException
-            );
+            Directory.Move(dirPath, newDirPath);
             return SimpleResult.Ok();
         }
         catch (Exception ex)
@@ -232,12 +180,7 @@ public class LinuxFileIOHandler : IFileIOHandler
         try
         {
             string newFilePath = Path.Combine(destinationDir, Path.GetFileName(filePath));
-            FileSystem.CopyFile(
-                filePath,
-                newFilePath,
-                UIOption.AllDialogs,
-                UICancelOption.ThrowException
-            );
+            File.Copy(filePath, newFilePath);
             return SimpleResult.Ok();
         }
         catch (Exception ex)
@@ -254,8 +197,8 @@ public class LinuxFileIOHandler : IFileIOHandler
             FileSystem.CopyDirectory(
                 dirPath,
                 newDirPath,
-                UIOption.AllDialogs,
-                UICancelOption.ThrowException
+                UIOption.OnlyErrorDialogs,
+                UICancelOption.DoNothing
             );
             return SimpleResult.Ok();
         }
@@ -267,19 +210,13 @@ public class LinuxFileIOHandler : IFileIOHandler
 
     public IResult DuplicateFile(string filePath, string copyName)
     {
-        bool showDialog = _fileInfoProvider.GetFileSizeB(filePath) > _showDialogLimit;
         try
         {
             if (Path.GetDirectoryName(filePath) is not string parentDir)
                 return SimpleResult.Error("Can't duplicate a root directory.");
 
             string newFilePath = Path.Combine(parentDir, copyName);
-            FileSystem.CopyFile(
-                filePath,
-                newFilePath,
-                showDialog ? UIOption.AllDialogs : UIOption.OnlyErrorDialogs,
-                UICancelOption.ThrowException
-            );
+            File.Copy(filePath, newFilePath);
             return SimpleResult.Ok();
         }
         catch (Exception ex)
