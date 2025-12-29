c9ac8294d96e0ec7803ef34bd72528217211361d
janecea <adam-janecek@seznam.cz>
2025-12-29 21:29:17 +0100

    Finish properties dialogue

--- src/FileSurfer.Linux/ViewModels/PropertiesVmFactory.cs
+++ src/FileSurfer.Linux/ViewModels/PropertiesVmFactory.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Globalization;
 using System.IO;
+using System.Linq;
 using System.Security;
 using Avalonia.Controls;
 using FileSurfer.Core.Models;
@@ -19,7 +20,13 @@ public sealed class PropertiesVmFactory : IPropertiesVmFactory
     private static string GetSize(FileSystemEntryViewModel entry, FileSystemInfo fsInfo)
     {
         if (!entry.IsDirectory)
-            return $"{entry.Size} ({entry.SizeB ?? 0}B)";
+        {
+            if (entry.Size is [.., ' ', 'B'])
+                return entry.Size;
+
+            string bytesFormatted = (entry.SizeB ?? 0).ToString("N0", CultureInfo.InvariantCulture);
+            return $"{entry.Size} ({bytesFormatted} B)";
+        }
 
         DirectoryInfo dirInfo = (DirectoryInfo)fsInfo;
         try
@@ -38,7 +45,11 @@ public sealed class PropertiesVmFactory : IPropertiesVmFactory
     {
         try
         {
-            return getDate().ToString(CultureInfo.CurrentCulture);
+            CultureInfo cultureInfo = CultureInfo.CurrentCulture;
+            DateTimeFormatInfo formatInfo = cultureInfo.DateTimeFormat;
+            string format = $"{formatInfo.LongDatePattern} {formatInfo.LongTimePattern}";
+
+            return getDate().ToString(format, cultureInfo);
         }
         catch (IOException)
         {
@@ -60,8 +71,11 @@ public sealed class PropertiesVmFactory : IPropertiesVmFactory
         return user == group ? user : $"{user}:{group}";
     }
 
-    private static string FormatPermissions(FileAccessPermissions perms)
+    private static string GetPermissions(string path)
     {
+        UnixFileInfo fileInfo = new(path);
+        FileAccessPermissions perms = fileInfo.FileAccessPermissions;
+
         char[] p = new char[9];
 
         p[0] = (perms & FileAccessPermissions.UserRead) != 0 ? 'r' : '-';
@@ -79,39 +93,40 @@ public sealed class PropertiesVmFactory : IPropertiesVmFactory
         return new string(p);
     }
 
-    private static string GetPermissions(string path)
+    private static ValueResult<FileSystemInfo> GetFileSystemInfo(FileSystemEntryViewModel entry)
     {
-        UnixFileInfo fileInfo = new(path);
-        FileAccessPermissions perms = fileInfo.FileAccessPermissions;
-
-        return FormatPermissions(perms);
+        try
+        {
+            return ValueResult<FileSystemInfo>.Ok(
+                entry.IsDirectory
+                    ? new DirectoryInfo(entry.PathToEntry)
+                    : new FileInfo(entry.PathToEntry)
+            );
+        }
+        catch (Exception ex)
+        {
+            return ValueResult<FileSystemInfo>.Error(ex.Message);
+        }
     }
 
     public ValueResult<IDisplayable> GetPropertiesVm(FileSystemEntryViewModel entry)
     {
-        FileSystemInfo fsInfo;
-        try
-        {
-            fsInfo = entry.IsDirectory
-                ? new DirectoryInfo(entry.PathToEntry)
-                : new FileInfo(entry.PathToEntry);
-        }
-        catch (Exception ex)
-        {
-            return ValueResult<IDisplayable>.Error(ex.Message);
-        }
+        ValueResult<FileSystemInfo> result = GetFileSystemInfo(entry);
+        if (!result.IsOk)
+            return ValueResult<IDisplayable>.Error($"Could not be access \"{entry.Name}\".");
 
-        if (!fsInfo.Exists)
-            return ValueResult<IDisplayable>.Error($"{entry.PathToEntry} does not exist.");
-
-        PropertiesWindowViewModel value = new(entry, _parentWindow)
+        FileSystemInfo fsInfo = result.Value;
+        PropertiesWindowViewModel value = new(
+            entry,
+            _parentWindow,
+            GetPermissions(entry.PathToEntry)
+        )
         {
             Size = GetSize(entry, fsInfo),
             DateCreated = GetDateString(() => fsInfo.CreationTime),
             DateAccessed = GetDateString(() => fsInfo.LastAccessTime),
             DateModified = GetDateString(() => fsInfo.LastWriteTime),
             Owner = GetOwner(entry.PathToEntry),
-            Permissions = GetPermissions(entry.PathToEntry),
         };
         return ValueResult<IDisplayable>.Ok(value);
     }
--- src/FileSurfer.Linux/ViewModels/PropertiesWindowViewModel.cs
+++ src/FileSurfer.Linux/ViewModels/PropertiesWindowViewModel.cs
@@ -1,10 +1,9 @@
-﻿using Avalonia.Controls;
+﻿using System;
+using Avalonia.Controls;
 using Avalonia.Media;
 using FileSurfer.Core.ViewModels;
-using FileSurfer.Core.Views;
 using FileSurfer.Linux.Models.Shell;
 using FileSurfer.Linux.Views;
-using ReactiveUI;
 
 namespace FileSurfer.Linux.ViewModels;
 
@@ -25,13 +24,58 @@ public sealed class PropertiesWindowViewModel : IDisplayable
     public required string DateAccessed { get; init; }
     public required string DateModified { get; init; }
     public required string Owner { get; init; }
-    public required string Permissions { get; init; }
+    public string PermissionsOwner { get; }
+    public string PermissionsGroup { get; }
+    public string PermissionsOther { get; }
 
-    public PropertiesWindowViewModel(FileSystemEntryViewModel entry, Window mainWindow)
+    public PropertiesWindowViewModel(
+        FileSystemEntryViewModel entry,
+        Window mainWindow,
+        string permissions
+    )
     {
         _entry = entry;
         _mainWindow = mainWindow;
+
+        ReadOnlySpan<char> perms = permissions.AsSpan();
+        if (entry.IsDirectory)
+        {
+            PermissionsOwner = GetDirPerms(perms[0..3]);
+            PermissionsGroup = GetDirPerms(perms[3..6]);
+            PermissionsOther = GetDirPerms(perms[6..9]);
+        }
+        else
+        {
+            PermissionsOwner = GetFilePerms(perms[0..3]);
+            PermissionsGroup = GetFilePerms(perms[3..6]);
+            PermissionsOther = GetFilePerms(perms[6..9]);
+        }
     }
+
+    private static string GetFilePerms(ReadOnlySpan<char> rwx) =>
+        rwx switch
+        {
+            ['r', 'w', 'x'] => "Can view, modify, and execute",
+            ['r', 'w', '-'] => "Can view and modify",
+            ['r', '-', 'x'] => "Can view and execute",
+            ['-', 'w', 'x'] => "Can modify and execute",
+            ['r', '-', '-'] => "Can view",
+            ['-', 'w', '-'] => "Can modify",
+            ['-', '-', 'x'] => "Can execute",
+            _ => "No permissions",
+        };
+
+    private static string GetDirPerms(ReadOnlySpan<char> rwx) =>
+        rwx switch
+        {
+            ['r', 'w', 'x'] => "Can view and modify contents",
+            ['r', 'w', '-'] => "Can view and modify contents",
+            ['r', '-', 'x'] => "Can view contents",
+            ['-', 'w', 'x'] => "Can modify contents",
+            ['-', '-', 'x'] => "Can access directory",
+            ['r', '-', '-'] => "Can view contents",
+            _ => "No permissions",
+        };
 
     public void Show()
     {
